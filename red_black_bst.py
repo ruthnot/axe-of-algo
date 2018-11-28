e6db2652a44983b3f9e71762d8d447a11bc4f5a9
ruthnot <liu.liu@tusimple.ai>
2018-11-27 23:32:24 -0800

    update red black bst

--- red_black_bst.py
+++ red_black_bst.py
@@ -22,5 +22,29 @@ class RedBlackBST:
         return x.color == RED
 
     def _rotate_left(self, h):
-        assert self._is_red(h)
+        assert self._is_red(h.right)
+        x = h.right
+        h.right = x.left
+        x.left = h
+        x.color = h.color
+        h.color = RED
+        return x
+
+    def _rotate_right(self, h):
+        assert self._is_red(h.left)
+        x = h.left
+        h.left = x.right
+        x.right = h
+        x.color = h.color
+        h.color = RED
+        return x
+
+    def _flip_colors(self, h):
+        assert not self._is_red(h)
+        assert self._is_red(h.left)
+        assert self._is_red(h.right)
+        h.color = RED
+        h.left.color = BLACK
+        h.left.color = BLACK
         
+
